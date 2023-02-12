6ec829cbf309349c49d8a6fefd4284129c431b19
Thierry Arnoux <thierry.arnoux@gmx.net>
2023-02-12 23:51:56 +0100

    Default to main RMM file

--- rumm/src/main.rs
+++ rumm/src/main.rs
@@ -16,9 +16,9 @@ fn run() -> Result {
     let app = clap_app!(("rumm") =>
         (version: crate_version!())
         (about: "A tactics based proof language for Metamath")
-        (@arg RMM_FILE: +required "Rumm file to load"));
+        (@arg RMM_FILE: "Rumm file to load"));
     let matches = app.get_matches();
-    let path = matches.value_of("RMM_FILE").unwrap();
+    let path = matches.value_of("RMM_FILE").unwrap_or("../set.rmm");
     let data = fs::read_to_string(path)
         .with_context(|| format!("could not read file `{}`", path))?;
     let mut script = Script::from_str(path.to_string(), &data)?;
